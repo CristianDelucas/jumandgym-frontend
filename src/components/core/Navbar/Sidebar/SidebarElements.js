dc14e1fb10c771ccde2dfd94da48d29965f4c5f6
CristianDelucas <crdelucasnoguero@gmail.com>
2022-04-11 17:54:33 +0200

    add update a links

--- src/components/core/Navbar/Sidebar/SidebarElements.js
+++ src/components/core/Navbar/Sidebar/SidebarElements.js
@@ -80,12 +80,10 @@ export const SidebarRoute = styled(LinkR)`
             padding:16px 64px;
             color: #010606;
             font-size:16px;
-            outline:none;
+            
             border:none;
             cursor:pointer;
             transition:all 0.2s ease-in-out;
-            text-decoration:none;
-
 
             &:hover{
                 transition:all 0.2s ease-in-out;
